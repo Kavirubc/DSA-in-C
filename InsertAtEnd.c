2b8f9807b9a42e6963f26a57c407e82fefb7497f
Kavirubc <hapuarachchikaviru@gmail.com>
2023-07-01 16:51:06 +0530

    Insert to end done

--- InsertAtEnd.c
+++ InsertAtEnd.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 int main()
 {
-    int arr[3];
+    int arr[3] = {1, 2, 3};
+    int size, n;
 
-    arr[0] = 1;
-    arr[1] = 2;
-    arr[2] = 3;
+    size = sizeof(arr) / sizeof(arr[0]);
+    int newValue = 9;
 
-    
+    int arr1[size + 1];
+
+    for (n = 0; n < size; n++)
+    {
+        arr1[n] = arr[n];
+    }
+    arr1[size] = newValue;
+    for (n = 0; n < size + 1; n++)
+    {
+        printf("%i", arr1[n]);
+    }
+    return 0;
 }
