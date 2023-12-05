d30c94636437e8d1cbc8343aa3305ff94a50598c
Kaviru Hapuarachchi <hapuarachchikaviru@gmail.com>
2023-12-05 15:45:24 +0530

    3 done

--- labSheet2/final/test11.c
+++ labSheet2/final/test11.c
@@ -7,7 +7,7 @@ struct Node
     struct Node *next;
 };
 
-//write a function to create a node at the begining of the link list
+//newNode at the begining
 struct Node *createNodeAtBegining(struct Node *head, int data)
 {
     struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
@@ -17,7 +17,22 @@ struct Node *createNodeAtBegining(struct Node *head, int data)
     return head;
 }
 
+//newNode at the end
+struct Node *createNodeAtEnd(struct Node *head, int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
 
+    struct Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+
+    return head;
+}
 
 struct Node *createNode(int data)
 {
@@ -70,6 +85,14 @@ int main()
     scanf("%d", &j);
     head = createNodeAtBegining(head, j);
     
+    printf("After inserted in the list : \n");
+    displayList(head);
+
+    int k;
+    printf("Input data for new node : ");
+    scanf("%d", &k);
+    head = createNodeAtEnd(head, k);
+
     printf("After inserted in the list : \n");
     displayList(head);
 
